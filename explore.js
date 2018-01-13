68246a1f6af9f2399400b7e20e911308bad41b0a
Stig Otnes Kolstad <stigok@broentech.no>
2018-01-13 19:17:45 +0100

    Use consistent code style

--- explore.js
+++ explore.js
@@ -3,7 +3,7 @@
 Interceptor.attach(ptr('0x55cac66ea6f0'), {
   onEnter: function (args){
     console.log('woot = ' + args[0].toInt32())
-    args[0] = args[0].add(9000)
+    args[0] = ptr(42)
   }
-});
+})
 
