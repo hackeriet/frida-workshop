9b01a1699328cae831ffb1ebb9a682c036b184a4
Stig Otnes Kolstad <stigok@broentech.no>
2018-01-13 20:04:44 +0100

    Use puts to send msg throug hello program

--- explore.js
+++ explore.js
@@ -1,11 +1,20 @@
 'use strict';
 
+var puts = new NativeFunction(
+  Module.findExportByName(null, 'puts'),
+  'int',
+  ['pointer'])
+
+function log (msg) {
+  puts(Memory.allocUtf8String(msg));
+}
+
 send({color: 'red'})
 
 recv(onMessage)
 
 function onMessage(msg) {
-  console.log('onMessage', JSON.stringify(msg, null, 2))
+  log('onMessage', JSON.stringify(msg, null, 2))
   recv(onMessage)
 }
 
