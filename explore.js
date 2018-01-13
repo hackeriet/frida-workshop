2c9b837f58246e920916ce66066f877cc9eebcd0
Stig Otnes Kolstad <stigok@broentech.no>
2018-01-13 19:25:22 +0100

    Replace interceptor attach with findExportByName

--- explore.js
+++ explore.js
@@ -1,9 +1,17 @@
 'use strict';
 
+/*
 Interceptor.attach(ptr('0x55cac66ea6f0'), {
   onEnter: function (args){
     console.log('woot = ' + args[0].toInt32())
     args[0] = ptr(42)
   }
 })
+*/
+
+var sleep = new NativeCallback(function (delay) {
+  Thread.sleep(0.05)
+}, 'uint', ['uint'])
+
+Interceptor.replace(Module.findExportByName(null, 'sleep'), sleep)
 
