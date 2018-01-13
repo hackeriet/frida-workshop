2d316596c6230e2cf088a6d4c17e272571534ecf
Stig Otnes Kolstad <stigok@broentech.no>
2018-01-13 18:53:11 +0100

    Add frida message handler

--- explore.py
+++ explore.py
@@ -7,7 +7,11 @@ session = frida.attach("hello")
 with codecs.open("explore.js", 'r', "utf-8") as f:
   source = f.read()
 
+def on_message(message, data):
+  print("on_message", message)
+
 script = session.create_script(source)
+script.on("message", on_message)
 script.load()
 
 sys.stdin.read()
